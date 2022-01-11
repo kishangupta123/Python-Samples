2569a2ced5410ab6c3cd41198daf453f11db06d5
kishangupta123 <96646948+kishangupta123@users.noreply.github.com>
2022-01-11 21:21:18 +0530

    Add files via upload

---
+++ let us C ch1 sol2.py
@@ -0,0 +1,11 @@
+m1=int(input("enter marks of first object"))
+m2=int(input("enter marks of second object"))
+m3=int(input("enter marks of third object"))
+m4=int(input("enter marks of fourth object"))
+m5=int(input("enter marks of fifth object"))
+total=(float)(m1+m2+m3+m4+m5)
+avg=total/5
+per=(total/500)*100
+print("The average mark obtained is:",avg)
+
+print(per)

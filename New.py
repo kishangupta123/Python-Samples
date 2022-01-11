cf55518e76aacb6663ef43927e50bdf0348b55b6
kishangupta123 <96646948+kishangupta123@users.noreply.github.com>
2022-01-11 20:25:15 +0530

    Add files via upload

---
+++ New.py
@@ -0,0 +1,10 @@
+
+distance = input("Entr distance in km:")
+meter = float(distance) * 1000
+feet = float(distance) * 3280.84
+inches = float(distance) * 39370.1
+centimeter = float(distance) * 100000
+print(meter)
+print(feet)
+print(inches)
+print(centimeter)
